496965c29d6df4c51c36a7b267811c6348116a9d
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2018-07-03 00:10:20 +0200

    Finish Implementation Chapter
    Change Implementation Chapter Figures

--- thesis/clean.sh
+++ thesis/clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dir=$(pwd)
+if [[ $# -gt 0 ]]; then
+  dir=$1
+fi
+
+cd $dir
 aux=(*.aux *.lof *.log *.lot *.fls *.out *.toc *.fmt *.fot *.cb *.cb2 *.synctex.gz *.bbl *.blg  *.dvi  *.fdb_latexmk)
 for file in ${aux[@]}; do
   if [[ -f $file ]]; then
@@ -7,3 +13,5 @@ for file in ${aux[@]}; do
     rm $file
   fi
 done
+
+cd -
